014978cf9f234448e7350174febe8dd2ab0079ce
Ashish Rajbanshi <ashish.rajbanshi@compliancequest.com>
2023-06-15 10:02:35 +0545

    [passed argument for source.]

--- build/projBuild.js
+++ build/projBuild.js
@@ -2,42 +2,62 @@ const fs = require('fs');
 const path = require('path');
 const { mkdirp } = require('mkdirp')
 
+// function copyFile(source, destination) {
+//     return new Promise((resolve, reject) => {
+//         const readStream = fs.createReadStream(Source, (err, data) => {
+//             //     if (err) {
+//             //         console.error(err);
+//             //         return;
+//             //     });
+
+//         const reg = /(\w+)(\__c)/g;
+//         const modifiedData = data.replace(reg, `compliancequest__$1$2`);
+//         const writeStream = fs.createWriteStream(destination, modifiedData);
+
+//         readStream.on('error', reject);
+//         writeStream.on('error', reject);
+
+//         writeStream.on('finish', resolve);
+//         readStream.pipe(writeStream);
+//     });
+// }
+
 function copyDirectory(source, destination) {
     if (!fs.existsSync(destination)) {
         mkdirp.sync(destination);
     }
 
-  const files = fs.readdirSync(source);
+    const files = fs.readdirSync(source);
 
-  files.forEach((file) => {
-    const sourcePath = path.join(source, file);
-    const destinationPath = path.join(destination, file);
+    files.forEach((file) => {
+        const sourcePath = path.join(source, file);
+        const destinationPath = path.join(destination, file);
 
-    if (fs.statSync(sourcePath).isDirectory()) {
-      copyDirectory(sourcePath, destinationPath);
-    } else {
-      fs.readFile(sourcePath, 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-          return;
+        if (fs.statSync(sourcePath).isDirectory()) {
+            copyDirectory(sourcePath, destinationPath);
+        } else {
+            fs.readFile(sourcePath, 'utf8', (err, data) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+
+                const reg = /(\w+)(\__c)/g;
+                const modifiedData = data.replace(reg, `compliancequest__$1$2`);
+
+                fs.writeFile(destinationPath, modifiedData, 'utf8', (err) => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                    console.log(`File "${destinationPath}" has been copied and modified.`);
+                });
+            });
         }
-
-        const reg = /(\w+)(\__c)/g;
-        const modifiedData = data.replace(reg,`compliancequest__$1$2`);
-
-        fs.writeFile(destinationPath, modifiedData, 'utf8', (err) => {
-          if (err) {
-            console.error(err);
-            return;
-          }
-          console.log(`File "${destinationPath}" has been copied and modified.`);
-        });
-      });
-    }
-  });
+    });
 }
 
-const sourceDir = '../force-app';
+const sourceDir = process.argv[2];
 const destinationDir = 'bin/force-app';
 
 copyDirectory(sourceDir, destinationDir);
