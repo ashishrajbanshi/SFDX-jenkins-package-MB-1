d3ee5898e5a7c6dc52abf6efb5284f66cf5e841b
Ashish Rajbanshi <46884226+ashishrajbanshi@users.noreply.github.com>
2023-08-04 11:46:29 +0545

    Update jenkinsfile_2.groovy

--- jenkinsfile_2.groovy
+++ jenkinsfile_2.groovy
@@ -1,1 +1,6 @@
-
+STRING_ENV =env.STRING_ENV
+node(){
+         def projectJson = readJSON file: 'sfdx-project.json';
+            readJson.replacements = STRING_ENV;
+            writeJSON file: 'sfdx-project.json', json: projectJson. pretty: 4;
+}
